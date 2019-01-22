c5aa8a273cb77fb247cdd216efd4603809d2cf60
gosha20777 <gosha20777@live.ru>
2019-01-22 17:07:41 +0300

    rewrite Tensor

--- src/tensor.h
+++ src/tensor.h
@@ -1,245 +1,162 @@
+#include <algorithm>
+#include <numeric>
 #include "utils.h"
+#include "reader.h"
 
 namespace keras2cpp {
     class Tensor {
-    public:
-        Tensor() {}
-
-        Tensor(int i) { Resize(i); }
-
-        Tensor(int i, int j) { Resize(i, j); }
-
-        Tensor(int i, int j, int k) { Resize(i, j, k); }
-
-        Tensor(int i, int j, int k, int l) { Resize(i, j, k, l); }
-
-        void Resize(int i) {
-            dims_ = {i};
-            data_.resize(i);
-        }
-
-        void Resize(int i, int j) {
-            dims_ = {i, j};
-            data_.resize(i * j);
-        }
-
-        void Resize(int i, int j, int k) {
-            dims_ = {i, j, k};
-            data_.resize(i * j * k);
-        }
-
-        void Resize(int i, int j, int k, int l) {
-            dims_ = {i, j, k, l};
-            data_.resize(i * j * k * l);
-        }
-
-        inline void Flatten() {
-            KDEBUG(dims_.size() > 0, "Invalid tensor");
-
-            int elements = dims_[0];
-            for (unsigned int i = 1; i < dims_.size(); i++) {
-                elements *= dims_[i];
-            }
-            dims_ = {elements};
-        }
-
-        inline float& operator()(int i) {
-            KDEBUG(dims_.size() == 1, "Invalid indexing for tensor");
-            KDEBUG(i < dims_[0] && i >= 0, "Invalid i: %d (max %d)", i, dims_[0]);
-
-            return data_[i];
-        }
-
-        inline float& operator()(int i, int j) {
-            KDEBUG(dims_.size() == 2, "Invalid indexing for tensor");
-            KDEBUG(i < dims_[0] && i >= 0, "Invalid i: %d (max %d)", i, dims_[0]);
-            KDEBUG(j < dims_[1] && j >= 0, "Invalid j: %d (max %d)", j, dims_[1]);
-
-            return data_[dims_[1] * i + j];
-        }
-
-        inline float operator()(int i, int j) const {
-            KDEBUG(dims_.size() == 2, "Invalid indexing for tensor");
-            KDEBUG(i < dims_[0] && i >= 0, "Invalid i: %d (max %d)", i, dims_[0]);
-            KDEBUG(j < dims_[1] && j >= 0, "Invalid j: %d (max %d)", j, dims_[1]);
-
-            return data_[dims_[1] * i + j];
-        }
-
-        inline float& operator()(int i, int j, int k) {
-            KDEBUG(dims_.size() == 3, "Invalid indexing for tensor");
-            KDEBUG(i < dims_[0] && i >= 0, "Invalid i: %d (max %d)", i, dims_[0]);
-            KDEBUG(j < dims_[1] && j >= 0, "Invalid j: %d (max %d)", j, dims_[1]);
-            KDEBUG(k < dims_[2] && k >= 0, "Invalid k: %d (max %d)", k, dims_[2]);
-
-            return data_[dims_[2] * (dims_[1] * i + j) + k];
-        }
-
-        inline float& operator()(int i, int j, int k, int l) {
-            KDEBUG(dims_.size() == 4, "Invalid indexing for tensor");
-            KDEBUG(i < dims_[0] && i >= 0, "Invalid i: %d (max %d)", i, dims_[0]);
-            KDEBUG(j < dims_[1] && j >= 0, "Invalid j: %d (max %d)", j, dims_[1]);
-            KDEBUG(k < dims_[2] && k >= 0, "Invalid k: %d (max %d)", k, dims_[2]);
-            KDEBUG(l < dims_[3] && l >= 0, "Invalid l: %d (max %d)", l, dims_[3]);
-
-            return data_[dims_[3] * (dims_[2] * (dims_[1] * i + j) + k) + l];
-        }
-
-        inline void Fill(float value) {
-            std::fill(data_.begin(), data_.end(), value);
-        }
-
-        Tensor Unpack(int row) const {
-            KASSERT(dims_.size() >= 2, "Invalid tensor");
-            std::vector<int> pack_dims =
-                std::vector<int>(dims_.begin() + 1, dims_.end());
-            int pack_size = std::accumulate(pack_dims.begin(), pack_dims.end(), 0);
-
-            std::vector<float>::const_iterator first =
-                data_.begin() + (row * pack_size);
-            std::vector<float>::const_iterator last =
-                data_.begin() + (row + 1) * pack_size;
-
-            Tensor x = Tensor();
-            x.dims_ = pack_dims;
-            x.data_ = std::vector<float>(first, last);
-
-            return x;
-        }
-
-        Tensor Select(int row) const {
-            Tensor x = Unpack(row);
-            x.dims_.insert(x.dims_.begin(), 1);
-
-            return x;
-        }
-
-        Tensor operator+(const Tensor& other) {
-            KASSERT(dims_ == other.dims_,
-                    "Cannot add tensors with different dimensions");
-
-            Tensor result;
-            result.dims_ = dims_;
-            result.data_.reserve(data_.size());
-
-            std::transform(data_.begin(), data_.end(), other.data_.begin(),
-                           std::back_inserter(result.data_),
-                           [](float x, float y) { return x + y; });
-
-            return result;
-        }
-
-        Tensor Multiply(const Tensor& other) {
-            KASSERT(dims_ == other.dims_,
-                    "Cannot multiply elements with different dimensions");
-
-            Tensor result;
-            result.dims_ = dims_;
-            result.data_.reserve(data_.size());
-
-            std::transform(data_.begin(), data_.end(), other.data_.begin(),
-                           std::back_inserter(result.data_),
-                           [](float x, float y) { return x * y; });
-
-            return result;
-        }
-
-        Tensor Dot(const Tensor& other) {
-            KDEBUG(dims_.size() == 2, "Invalid tensor dimensions");
-            KDEBUG(other.dims_.size() == 2, "Invalid tensor dimensions");
-            KASSERT(dims_[1] == other.dims_[0],
-                    "Cannot multiply with different inner dimensions");
-
-            Tensor tmp(dims_[0], other.dims_[1]);
-
-            for (int i = 0; i < dims_[0]; i++) {
-                for (int j = 0; j < other.dims_[1]; j++) {
-                    for (int k = 0; k < dims_[1]; k++) {
-                        tmp(i, j) += (*this)(i, k) * other(k, j);
-                    }
-                }
+        public:
+            Tensor() = default;
+            template <
+                typename... Size,
+                typename = std::enable_if_t<(... && std::is_integral_v<Size>)>>
+            Tensor(Size... sizes) {
+                resize(static_cast<size_t>(sizes)...);
             }
 
-            return tmp;
-        }
+            Tensor(Stream& file, size_t rank = 1);
 
-        Tensor Fma(const Tensor& scale, const Tensor& bias) const noexcept {
-            KASSERT(dims_ == scale.dims_, "Invalid tensor dimensions");
-            KASSERT(dims_ == bias.dims_, "Invalid tensor dimensions");
+            template <typename... Size>
+            static auto empty(Size... sizes);
 
-            Tensor result;
-            result.dims_ = dims_;
-            result.data_.resize(data_.size());
+            template <typename... Size>
+            void resize(Size... sizes) noexcept;
 
-            auto k_ = scale.data_.begin();
-            auto b_ = bias.data_.begin();
-            auto r_ = result.data_.begin();
-            for (auto x_ = data_.begin(); x_ != data_.end();)
-                *(r_++) = *(x_++) * *(k_++) + *(b_++);
-            return result;
-        }
+            inline size_t size() const noexcept;
+            inline size_t ndim() const noexcept;
+            inline Tensor& flatten() noexcept;
 
-        void Print() {
-            if (dims_.size() == 1) {
-                printf("[ ");
-                for (int i = 0; i < dims_[0]; i++) {
-                    printf("%f ", (*this)(i));
-                }
-                printf("]\n");
-            } else if (dims_.size() == 2) {
-                printf("[\n");
-                for (int i = 0; i < dims_[0]; i++) {
-                    printf(" [ ");
-                    for (int j = 0; j < dims_[1]; j++) {
-                        printf("%f ", (*this)(i, j));
-                    }
-                    printf("]\n");
-                }
-                printf("]\n");
-            } else if (dims_.size() == 3) {
-                printf("[\n");
-                for (int i = 0; i < dims_[0]; i++) {
-                    printf(" [\n");
-                    for (int j = 0; j < dims_[1]; j++) {
-                        printf("  [ ");
-                        for (int k = 0; k < dims_[2]; k++) {
-                            printf("%f ", (*this)(i, j, k));
-                        }
-                        printf("  ]\n");
-                    }
-                    printf(" ]\n");
-                }
-                printf("]\n");
-            } else if (dims_.size() == 4) {
-                printf("[\n");
-                for (int i = 0; i < dims_[0]; i++) {
-                    printf(" [\n");
-                    for (int j = 0; j < dims_[1]; j++) {
-                        printf("  [\n");
-                        for (int k = 0; k < dims_[2]; k++) {
-                            printf("   [");
-                            for (int l = 0; l < dims_[3]; l++) {
-                                printf("%f ", (*this)(i, j, k, l));
-                            }
-                            printf("]\n");
-                        }
-                        printf("  ]\n");
-                    }
-                    printf(" ]\n");
-                }
-                printf("]\n");
-            }
-        }
+            inline float& operator()(size_t) noexcept;
+            inline float& operator()(size_t, size_t) noexcept;
+            inline float& operator()(size_t, size_t, size_t) noexcept;
+            inline float& operator()(size_t, size_t, size_t, size_t) noexcept;
+            inline float operator()(size_t) const noexcept;
+            inline float operator()(size_t, size_t) const noexcept;
+            inline float operator()(size_t, size_t, size_t) const noexcept;
+            inline float operator()(size_t, size_t, size_t, size_t) const noexcept;
 
-        void PrintShape() {
-            printf("(");
-            for (unsigned int i = 0; i < dims_.size(); i++) {
-                printf("%d ", dims_[i]);
-            }
-            printf(")\n");
-        }
+            inline std::vector<float>::iterator begin() noexcept;
+            inline std::vector<float>::const_iterator begin() const noexcept;
+            inline std::vector<float>::iterator end() noexcept;
+            inline std::vector<float>::const_iterator end() const noexcept;
 
-        std::vector<int> dims_;
-        std::vector<float> data_;
+            inline void fill(float value) noexcept;
+
+            Tensor unpack(size_t row) const noexcept;
+            Tensor select(size_t row) const noexcept;
+
+            Tensor& operator+=(const Tensor& other) noexcept;
+            Tensor& operator*=(const Tensor& other) noexcept;
+            Tensor fma(const Tensor& scale, const Tensor& bias) const noexcept;
+            Tensor dot(const Tensor& other) const noexcept;
+
+            void print() const noexcept;
+            void print_shape() const noexcept;
+
+            std::vector<size_t> dims_;
+            std::vector<float> data_;
     };
+
+    template <typename... Size>
+    auto Tensor::empty(Size... sizes) {
+        Tensor tensor;
+        tensor.dims_ = {static_cast<size_t>(sizes)...};
+        tensor.data_.reserve(tensor.size());
+        return tensor;
+    }
+    template <typename... Size>
+    void Tensor::resize(Size... sizes) noexcept {
+        dims_ = {static_cast<size_t>(sizes)...};
+        data_.resize(size());
+    }
+    size_t Tensor::size() const noexcept {
+        size_t elements = 1;
+        for (const auto& it : dims_)
+            elements *= it;
+        return elements;
+    }
+    size_t Tensor::ndim() const noexcept {
+        return dims_.size();
+    }
+    Tensor& Tensor::flatten() noexcept {
+        KASSERT(ndim());
+        dims_ = {size()};
+        return *this;
+    }
+    float& Tensor::operator()(size_t i) noexcept {
+        KASSERT(ndim() == 1);
+        KASSERT(i < dims_[0]);
+        return data_[i];
+    }
+    float Tensor::operator()(size_t i) const noexcept {
+        KASSERT(ndim() == 1);
+        KASSERT(i < dims_[0]);
+        return data_[i];
+    }
+    float& Tensor::operator()(size_t i, size_t j) noexcept {
+        KASSERT(ndim() == 2);
+        KASSERT(i < dims_[0]);
+        KASSERT(j < dims_[1]);
+        return data_[dims_[1] * i + j];
+    }
+    float Tensor::operator()(size_t i, size_t j) const noexcept {
+        KASSERT(ndim() == 2);
+        KASSERT(i < dims_[0]);
+        KASSERT(j < dims_[1]);
+        return data_[dims_[1] * i + j];
+    }
+    float& Tensor::operator()(size_t i, size_t j, size_t k) noexcept {
+        KASSERT(ndim() == 3);
+        KASSERT(i < dims_[0]);
+        KASSERT(j < dims_[1]);
+        KASSERT(k < dims_[2]);
+        return data_[dims_[2] * (dims_[1] * i + j) + k];
+    }
+    float Tensor::operator()(size_t i, size_t j, size_t k) const noexcept {
+        KASSERT(ndim() == 3);
+        KASSERT(i < dims_[0]);
+        KASSERT(j < dims_[1]);
+        KASSERT(k < dims_[2]);
+        return data_[dims_[2] * (dims_[1] * i + j) + k];
+    }
+    float& Tensor::operator()(size_t i, size_t j, size_t k, size_t l) noexcept {
+        KASSERT(ndim() == 4);
+        KASSERT(i < dims_[0]);
+        KASSERT(j < dims_[1]);
+        KASSERT(k < dims_[2]);
+        KASSERT(l < dims_[3]);
+        return data_[dims_[3] * (dims_[2] * (dims_[1] * i + j) + k) + l];
+    }
+    float Tensor::operator()(size_t i, size_t j, size_t k, size_t l) const
+        noexcept {
+        KASSERT(ndim() == 4);
+        KASSERT(i < dims_[0]);
+        KASSERT(j < dims_[1]);
+        KASSERT(k < dims_[2]);
+        KASSERT(l < dims_[3]);
+        return data_[dims_[3] * (dims_[2] * (dims_[1] * i + j) + k) + l];
+    }
+    void Tensor::fill(float value) noexcept {
+        std::fill(begin(), end(), value);
+    }
+    std::vector<float>::iterator Tensor::begin() noexcept {
+        return data_.begin();
+    }
+    std::vector<float>::const_iterator Tensor::begin() const noexcept {
+        return data_.begin();
+    }
+    std::vector<float>::iterator Tensor::end() noexcept {
+        return data_.end();
+    }
+    std::vector<float>::const_iterator Tensor::end() const noexcept {
+        return data_.end();
+    }
+    inline Tensor operator+(Tensor lhs, const Tensor& rhs) noexcept {
+        lhs += rhs;
+        return lhs;
+    }
+    inline Tensor operator*(Tensor lhs, const Tensor& rhs) noexcept {
+        lhs *= rhs;
+        return lhs;
+    }    
 }
